99c4c0da3df1974fe4b1876cf3f5d39def13ee82
G <g@katakate.org>
2025-10-23 22:03:01 +0300

    bump to 0.0.4-dev to avoid local build conflicts

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="katakate",
-    version="0.0.3",
+    version="0.0.4-dev",
     description="Katakate Sandbox Management Python SDK",
     packages=find_packages(where="src", include=["katakate", "katakate.*"]),
     package_dir={"": "src"},
